5ca880cbf6477d7aa2eff3922a799cb9aaedd0d3
yehordanilenko <egordanilenkooo@gmail.com>
2024-03-15 15:00:33 +0100

    Initial commit

---
+++ Lab4/Program.cs
@@ -0,0 +1,22 @@
+﻿using Lab4;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        double a1 = 1;
+        double b1 = 3;
+        double a2 = -2;
+        double b2 = -1;
+
+        Straight AB = new Straight(a1, b1);
+        Straight CD = new Straight(a2, b2);
+
+        Console.WriteLine(AB.GetIntersections());
+        Console.WriteLine(CD.GetIntersections());
+        Console.WriteLine(AB != CD);//Перпендикулярны ли прямые?
+        Console.WriteLine(AB % CD); //Угол между прямыми в градусах
+         
+    }
+    
+}
---
+++ Lab4/Straight.cs
@@ -0,0 +1,47 @@
+﻿namespace Lab4;
+
+public class Straight
+{
+    private double a;
+    private double b;
+
+    // Конструктор
+    public Straight(double a, double b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+
+    // Метод вывода уравнения прямой
+    public override string ToString()
+    {
+        return $"Straight line y = {a}x + ({b})";
+    }
+    
+    // Определение точек пересечения с осями
+    public string GetIntersections()
+    {
+        double xIntersection = -b / a;
+        double yIntersection = b;
+        return $"Intersection for line y={a}x+({b}) with abscissa is ({xIntersection}, 0), with ordinate is (0, {yIntersection})";
+    }
+    
+    // Определение перпендикулярности
+    public static bool operator !=(Straight l1, Straight l2)
+    {
+        return l1.a * l2.a == -1;
+    }
+
+    public static bool operator ==(Straight l1, Straight l2)
+    {
+        return l1.a.Equals(l2.a) && l1.b.Equals(l2.b);
+    }
+    
+    // Определение угла между прямыми
+    public static double operator %(Straight l1, Straight l2)
+    {
+        double angleRadians = Math.Atan(Math.Abs((l1.a - l2.a) / (1 + l1.a * l2.a))); // Формула для вычисления арктангенса угла между прямыми
+        return Math.Round(angleRadians * (180 / Math.PI), 2); // Конвертация в градусы
+    }
+    
+}
